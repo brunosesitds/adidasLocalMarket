69619d2b74d9bd78f245b8be3be8054025c462ac
Otávio Martineschen <otaviomart7tds@gmail.com>
2026-06-30 08:29:08 -0300

    Add files via upload

--- src/carrinho/script.js
+++ src/carrinho/script.js
@@ -8,17 +8,61 @@ $(document).ready(function(){
         listElement.empty()
         let totalPreco = 0
         $.each(carrinho, function(index, item){
-            const listItem = $("<li>").text(`${item.desc} - Preço: $${item.preco.toFixed(2)}`)
+            const listItem = $("<li>").text(`${item.desc} - Preço: $${item.valor.toFixed(2)}`)
             const removeButton = $("button").text("✖️").css("margin-left", "10px").click(function(){
                 removerItem(index)
             })
-            //appends filhos aq
+            listItem.append(removeButton)
+            listElement.append(listItem)
+
+            totalPreco += item.valor
         })
+        totalElement.text(`Total: $${totalPreco.toFixed(2)}`)
     }
 
-    //função remover item
+    function removerItem(index){
+        carrinho.splice(index, 1)
+        localStorage.setItem("carrinho", JSON.stringify(carrinho))
+        exibirCarrinho()
+    }
+
+        exibirCarrinho()
 })
 
-//gerar word
+function gerar(){
+    const listaElement = document.getElementById("lista")
+    const totalElement = document.getElementById("total")
+    const listaClone = listaElement.cloneNode(true)
+    $(listaClone).find("button").remove()
+    const listaHtml = listaClone.innerHTML
+    const totalHtml = totalElement.innerHTML
+    const conteudoHTML = `
+        <html>
+            <head>
+                <meta charset="UTF-8">
+            </head>
+            <body>
+                <h1>PEDIDO CONFIRMADO</h1>
+                <h3>Agradecemos a sua compra e sua preferência.</h3>
+                <br>
+                ${listaHtml}
+                <br>
+                <br>
+                ${totalHtml}
+            </body>
+        </html>
+    `
 
-//succeses close
+    const blob = new Blob([conteudoHTML], {type: "aplication/nsword"})
+    const link = document.createElement("a")
+
+    link.href = URL.createObjectURL(blob)
+    link.download = "pedido.doc"
+    link.click()
+    document.getElementById("pedido").style.display = "block"
+}
+
+
+function sucessClose(){
+    document.getElementById("pedido").style.display = "none"
+}
